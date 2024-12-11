03ecde90dc0634dd57f81ffd4edf09623261e6ed
Uriel Campaña <urielcamp@gmail.com>
2024-12-11 17:58:36 -0300

    Ejercicio 1 y 2 de condicionales, seccion faciles

--- src/Condicionales.java
+++ src/Condicionales.java
@@ -1,5 +1,36 @@
 public class Condicionales {
 
+    public static void main(String[] args) {
+
+
+        // EJERCICIOS FACILES
+
+        System.out.println(" ");
+        System.out.println("-----------------------------------------");
+        System.out.println(" ");
+
+
+        //Escribe un programa que solicite un número e imprima si es positivo.
+
+        int num = 4;
+
+        if(num > 0){
+            System.out.println("es positivo");
+        }
+
+        System.out.println(" ");
+        System.out.println("-----------------------------------------");
+        System.out.println(" ");
+
+        //Solicita un número y verifica si es impar.
+
+        if (num%2 == 0){
+            System.out.println(num + " es par");
+        }else {
+            System.out.println(num + " es impar");
+        }
+
+    }
 
 
 
